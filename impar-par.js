ec8703b7e3df5dd2b35fce4e9d78a2b188e3e722
brsphoenix <luismagno070@gmail.com>
2026-01-28 17:11:16 -0300

    finalizado sem dificuldades, prática de conceitos básicos de JS

---
+++ impar-par.js
@@ -0,0 +1,33 @@
+// receber uma entrada, verifiar se é impar ou par, retornar , dizer se  é maior que 10
+// dizer se é positivo ou negativo
+
+let entrada = 0
+
+if(entrada%2 !== 0){
+    console.log("O número é ímpar.")
+}
+
+ else{ console.log("O número é par.") 
+
+ }
+
+ if(entrada > 10){
+    console.log("O número é maior que 10.")
+
+ }
+  else{
+    console.log("O número é menor que 10.")
+  }
+
+  if(entrada < 0){
+    console.log("O número é negativo.")
+  }
+  else if(entrada === 0){
+
+    console.log("O número é zero.")
+  }
+
+  else{
+
+    console.log("O número é positivo.")
+  }
