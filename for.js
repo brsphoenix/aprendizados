fa28f8674e2811082e8b7f93d549e6d8ea28046b
brsphoenix <luismagno070@gmail.com>
2026-01-28 17:58:45 -0300

    finalizado sem dificuldades, apenas fiquei um pouco confuso porque tinha esquecido alguns conceitos

---
+++ for.js
@@ -0,0 +1,18 @@
+//imprimir numeros de 1 a 10, imprimir apenas os pares, somar todos os números
+
+
+
+
+//Vou comentar abaixo para não atrapalhar o desenvolvimento dos outros conceitos
+
+
+//for (let i = 1 ;i < 10; i++) {console.log(i)}
+
+
+
+//for (let i = 2 ;i <= 20; i+=2) {console.log(i)}
+
+let numero = 5
+for (let i = 1; i <= 10; i++){
+    console.log(numero + " x " + i + " = " + (numero * i))
+}
