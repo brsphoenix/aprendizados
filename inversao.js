bb3260fe76f7a3f19774537c661cb47b8e8922e9
brsphoenix <luismagno070@gmail.com>
2026-02-04 09:27:31 -0300

    finalizado apenas com umas dificuldades de raciocinio que foram desenvolvidas

--- inversao.js
+++ inversao.js
@@ -1,5 +1,15 @@
- // UM proama vai receber dois valores : a = 7 e b = 8;
+ // UM programa vai receber dois valores : a = 7 e b = 8;
    // o programa deve inverter os valores de a e b
    // a = 8 e b = 7;
+
+   let a = 8
+
+   let b = 7
+
+   let inversao = a
+
+   a = b
+
+   b = inversao
 
    
