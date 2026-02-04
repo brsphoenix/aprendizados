f28ac8732fdfa3223eb00d51a294d1fce465f239
brsphoenix <luismagno070@gmail.com>
2026-02-04 10:00:22 -0300

    finalizado

--- arrays.js
+++ arrays.js
@@ -1,2 +1,44 @@
 // criar um array de numeros, filtrar numeros pares, 
 // somar os numeros ímpares, encontrar o maior valor
+
+
+let numeros = [ 1,2,3,4,5,6,7,8,9,10]
+
+
+for (let i = 0; i < numeros.length ; i++){
+
+    console.log(numeros[i])
+}
+
+
+for (let par = 0; par < numeros.length; par++){
+
+
+if (numeros[par] % 2 === 0){
+
+    console.log(numeros[par])
+}
+else{}
+}
+let soma = 0
+for (let imparSoma = 0; imparSoma < numeros.length; imparSoma++){
+
+if (numeros[imparSoma] %2 !== 0){
+soma = soma + numeros[imparSoma]
+console.log(soma)
+}
+
+}
+
+
+let maior = numeros[0];
+
+for (let n = 0; n < numeros.length; n++){
+
+if (numeros[n] > maior){
+    maior = numeros[n]
+
+}
+
+}
+console.log(maior)
