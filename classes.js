f324a91f3c13f6de942691d56618fc29998b6344
brsphoenix <luismagno070@gmail.com>
2026-02-06 10:01:46 -0300

    finalizado sem dificuldade alguma

--- classes.js
+++ classes.js
@@ -1,2 +1,21 @@
 //crie uma classe pessoa,contendo, nome, idade e metodo apresentar()
 
+class pessoa {
+
+constructor(nome, idade){
+
+    this.nome = nome
+    this.idade = idade
+}
+apresentar(){
+
+    console.log(`Olá, me chamo ${this.nome} e tenho, ${this.idade} anos.`)
+}
+
+
+
+}
+
+let person = new pessoa("Luís",25)
+
+person.apresentar()
